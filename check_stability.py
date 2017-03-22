c93a02259e547d977bba4a3b625c1190f347009a
James Graham <james@hoppipolla.co.uk>
2017-03-22 12:18:39 +0000

    Stop the stability checker running on multiple commits on master.
    The stability checker tried to work out which commits to test by
    looking at all the commits on the current branch that aren't on
    another branch. But if the current branch is e.g. master then this
    will produce many more commits than intended. To fix this use the
    merge-base with master when it's closer to HEAD than the last commit
    not on another branch.

--- check_stability.py
+++ check_stability.py
@@ -408,6 +408,15 @@ def get_branch_point(user):
         commits = git("rev-list", "HEAD", *not_heads).split("\n")
         first_commit = commits[-1]
         branch_point = git("rev-parse", first_commit + "^")
+        # The above can produce a too-early commit if we are e.g. on master and there are
+        # preceeding changes that were rebased and so aren't on any other branch. To avoid
+        # this issue we check for the later of the above branch point and the merge-base
+        # with master
+        merge_base = git("merge-base", "HEAD", "origin/master")
+        if (branch_point != merge_base and
+            not git("log", "-oneline", "%s..%s" % (merge_base, branch_point)).strip()):
+            branch_point = merge_base
+
     logger.debug("Branch point from master: %s" % branch_point)
     return branch_point
 
